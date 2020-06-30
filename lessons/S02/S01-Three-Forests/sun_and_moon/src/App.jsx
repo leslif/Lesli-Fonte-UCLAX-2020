b37002155f2fd87f21a561a86eaf6d86b2ba0eda
Lesli Fonte <leslifonte@ProLaptop.local>
2020-06-29 18:34:36 -0700

    Added S01 homework assignment

---
+++ lessons/S02/S01-Three-Forests/sun_and_moon/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+import SunAndMoon from './SunAndMoon.jsx';
+
+
+const App = () => {
+    return (
+        <div>
+            App has changed
+            <SunAndMoon />
+        </div>
+    );
+}
+
+export default App;
